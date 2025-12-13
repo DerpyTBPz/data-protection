dd84ce8310d1e63ef06209e24377e5686088f30b
derpytbpz <dima.plyasov13@gmail.com>
2025-12-13 14:16:02 +0200

    lab8

---
+++ lab8/main.c
@@ -0,0 +1,233 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+#define AES_BLOCK_SIZE 16
+
+static const uint8_t sbox[256] = {
+    0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
+    0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+    0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+    0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+    0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+    0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+    0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+    0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+    0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+    0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+    0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+    0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+    0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+    0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+    0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+    0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16 
+};
+
+static const uint8_t Rcon[11] = {
+    0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36 
+};
+
+#define getSBoxValue(num) (sbox[(num)])
+
+void KeyExpansion(const uint8_t* inputKey, uint8_t* roundKeys)
+{
+    int i, j, k;
+    uint8_t tempa[4]; 
+
+    for (i = 0; i < 4; i++)
+    {
+        roundKeys[i * 4] = inputKey[i * 4];
+        roundKeys[i * 4 + 1] = inputKey[i * 4 + 1];
+        roundKeys[i * 4 + 2] = inputKey[i * 4 + 2];
+        roundKeys[i * 4 + 3] = inputKey[i * 4 + 3];
+    }
+
+    for (i = 4; i < 4 * 11; i++)
+    {
+        k = (i - 1) * 4;
+        tempa[0] = roundKeys[k + 0];
+        tempa[1] = roundKeys[k + 1];
+        tempa[2] = roundKeys[k + 2];
+        tempa[3] = roundKeys[k + 3];
+
+        if (i % 4 == 0)
+        {
+            const uint8_t u8tmp = tempa[0];
+            tempa[0] = tempa[1];
+            tempa[1] = tempa[2];
+            tempa[2] = tempa[3];
+            tempa[3] = u8tmp;
+
+            tempa[0] = getSBoxValue(tempa[0]);
+            tempa[1] = getSBoxValue(tempa[1]);
+            tempa[2] = getSBoxValue(tempa[2]);
+            tempa[3] = getSBoxValue(tempa[3]);
+
+            tempa[0] = tempa[0] ^ Rcon[i/4];
+        }
+
+        j = i * 4; k = (i - 4) * 4;
+        roundKeys[j + 0] = roundKeys[k + 0] ^ tempa[0];
+        roundKeys[j + 1] = roundKeys[k + 1] ^ tempa[1];
+        roundKeys[j + 2] = roundKeys[k + 2] ^ tempa[2];
+        roundKeys[j + 3] = roundKeys[k + 3] ^ tempa[3];
+    }
+}
+
+void AddRoundKey(uint8_t round, uint8_t* state, const uint8_t* roundKeys)
+{
+    for (int i = 0; i < 16; ++i)
+    {
+        state[i] ^= roundKeys[(round * 16) + i];
+    }
+}
+
+void SubBytes(uint8_t* state)
+{
+    for (int i = 0; i < 16; i++)
+    {
+        state[i] = getSBoxValue(state[i]);
+    }
+}
+
+void ShiftRows(uint8_t* state)
+{
+    uint8_t temp[16];
+    int i;
+    for(i=0;i<16;i++) temp[i] = state[i];
+
+    state[1] = temp[5]; state[5] = temp[9]; state[9] = temp[13]; state[13] = temp[1];
+    state[2] = temp[10]; state[6] = temp[14]; state[10] = temp[2]; state[14] = temp[6];
+    state[3] = temp[15]; state[7] = temp[3]; state[11] = temp[7]; state[15] = temp[11];
+}
+
+uint8_t xtime(uint8_t x)
+{
+    return ((x << 1) ^ (((x >> 7) & 1) * 0x1b));
+}
+
+void MixColumns(uint8_t* state)
+{
+    int i;
+    uint8_t Tmp, Tm, t;
+    for (i = 0; i < 16; i += 4)
+    {
+        t = state[i];
+        Tmp = state[i] ^ state[i+1] ^ state[i+2] ^ state[i+3];
+        Tm = state[i] ^ state[i+1]; Tm = xtime(Tm); state[i] ^= Tm ^ Tmp;
+        Tm = state[i+1] ^ state[i+2]; Tm = xtime(Tm); state[i+1] ^= Tm ^ Tmp;
+        Tm = state[i+2] ^ state[i+3]; Tm = xtime(Tm); state[i+2] ^= Tm ^ Tmp;
+        Tm = state[i+3] ^ t; Tm = xtime(Tm); state[i+3] ^= Tm ^ Tmp;
+    }
+}
+
+void AES_EncryptBlock(const uint8_t* input, uint8_t* output, const uint8_t* roundKeys)
+{
+    uint8_t state[16];
+    int i;
+
+    for (i = 0; i < 16; i++) state[i] = input[i];
+
+    AddRoundKey(0, state, roundKeys);
+
+    for (uint8_t round = 1; round < 10; ++round)
+    {
+        SubBytes(state);
+        ShiftRows(state);
+        MixColumns(state);
+        AddRoundKey(round, state, roundKeys);
+    }
+
+    SubBytes(state);
+    ShiftRows(state);
+    AddRoundKey(10, state, roundKeys);
+
+    for (i = 0; i < 16; i++) output[i] = state[i];
+}
+
+void ctr_increment_counter(uint8_t* counter)
+{
+    for (int i = 15; i >= 0; i--)
+    {
+        counter[i]++;
+        if (counter[i] != 0)
+        {
+            break;
+        }
+    }
+}
+
+void AES_CTR_xcrypt(const uint8_t* key, const uint8_t* iv, const uint8_t* input, uint8_t* output, size_t length)
+{
+    uint8_t roundKeys[176];
+    uint8_t counter_block[16];
+    uint8_t keystream_block[16];
+
+    KeyExpansion(key, roundKeys);
+    memcpy(counter_block, iv, 16);
+
+    size_t i = 0;
+    while (i < length)
+    {
+        AES_EncryptBlock(counter_block, keystream_block, roundKeys);
+        ctr_increment_counter(counter_block);
+
+        size_t chunk_size = (length - i < 16) ? (length - i) : 16;
+        
+        for (size_t j = 0; j < chunk_size; j++)
+        {
+            output[i + j] = input[i + j] ^ keystream_block[j];
+        }
+
+        i += chunk_size;
+    }
+}
+
+void print_hex(const char* label, const uint8_t* data, size_t len)
+{
+    printf("%s: ", label);
+    for (size_t i = 0; i < len; i++)
+    {
+        printf("%02x ", data[i]);
+    }
+    printf("\n");
+}
+
+void print_char(const char* label, const uint8_t* data, size_t len)
+{
+    printf("%s: ", label);
+    for (size_t i = 0; i < len; i++)
+    {
+        printf("%c", data[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    uint8_t key[16] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 
+                        0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
+
+    uint8_t iv[16]  = { 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 
+                        0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff };
+
+    const char* message = "Hello, AES-CTR Mode! This text is longer than 16 bytes.";
+    size_t len = strlen(message);
+
+    uint8_t encrypted[128];
+    uint8_t decrypted[128];
+
+    printf("=== AES-128 CTR Mode Demo ===\n\n");
+    print_char("Original", (uint8_t*)message, len);
+
+    AES_CTR_xcrypt(key, iv, (uint8_t*)message, encrypted, len);
+    print_hex("Encrypted", encrypted, len);
+
+    AES_CTR_xcrypt(key, iv, encrypted, decrypted, len);
+    decrypted[len] = '\0';
+    
+    print_hex("Decrypted Hex", decrypted, len);
+    print_char("Decrypted Txt", decrypted, len);
+
+    return 0;
+}
